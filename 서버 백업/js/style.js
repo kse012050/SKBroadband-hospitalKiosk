131b45e625b605b0497de276569b0bcc4308613b
김성은 <kse012050@naver.com>
2024-05-13 10:57:03 +0900

    서버 자료 백업

---
+++ 서버 백업/js/style.js
@@ -0,0 +1,99 @@
+$(document).ready(function(){
+    // 기본 - 화면의 가로, 세로 크기 / 스크롤 존재가 있다면 스크롤 크기 없으면 0
+    basicStyle();
+    $(window).resize(function(){
+        basicStyle();
+    })
+
+    // 스타일 인덱스
+    styleIdx();
+
+    $(window).scroll(function(){
+        $(this).scrollTop() > 0 ? $('header').addClass('active') : $('header').removeClass('active');
+    })
+
+    // 팝업
+    popup();
+    // 팝업
+    function popup(){
+        $('[data-popupOpen]').click(function(){
+            const attrName = $(this).attr('data-popupOpen');
+            $(`[data-popup="${attrName}"]`).addClass('active');
+        })
+        $('[data-popupClose]').click(function(){
+            const attrName = $(this).attr('data-popupClose');
+            $(`[data-popup="${attrName}"]`).removeClass('active');
+        })
+        $('[data-popup]').click(function(){
+            $(this).removeClass('active');
+        })
+        $('[data-popup] > div').click(function(e){
+            e.stopPropagation();
+        })
+    }
+
+    // 슬라이더
+    slider()
+})
+
+// 기본 - 화면의 가로, 세로 크기 / 스크롤 존재가 있다면 스크롤 크기 없으면 0
+function basicStyle(){
+    $('body').css('--fullHeight', window.innerHeight + 'px');
+    $('body').css('--fullWidth', window.innerWidth + 'px');
+    let scrollWidth;
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        scrollWidth = '0'
+    } else {
+        scrollWidth = '17'
+    }
+    $('body').css('--scrollWidth', scrollWidth + 'px');
+}
+
+// 스타일 인덱스
+function styleIdx(){
+    $('[data-styleIdx]').each(function(){
+        const childrenSelect = $(this).attr('data-styleIdx') ? $(this).children($(this).attr('data-styleIdx')) : $(this).children();
+        $(this).css('--idxTotal', childrenSelect.length)
+        childrenSelect.each(function(i){
+            $(this).css('--styleIdx', i)
+        })
+    })
+}
+
+
+// 팝업
+function popup(){
+    $('[data-popupOpen]').click(function(){
+        const attrName = $(this).attr('data-popupOpen');
+        $(`[data-popup="${attrName}"]`).addClass('active');
+    })
+    $('[data-popupClose]').click(function(){
+        const attrName = $(this).attr('data-popupClose');
+        $(`[data-popup="${attrName}"]`).removeClass('active');
+    })
+    $('[data-popup]').click(function(){
+        $(this).removeClass('active');
+    })
+    $('[data-popup]').click(function(e){
+        e.stopPropagation();
+    })
+}
+
+// 슬라이더
+function slider(){
+    var swiper = new Swiper(".mySwiper", {
+        // effect: "fade",
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        
+        centeredSlides: true,
+        autoplay: {
+            delay: 4000,
+            // disableOnInteraction: true
+        },
+        fadeEffect: {
+            crossFade: true
+        },
+    });
+}
